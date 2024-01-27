ed208a622a434052c15d618b3aeaef8067dae7ce
bakha2209 <bakhodir2209@gmail.com>
2024-01-27 16:29:46 +0500

    feat: develop routing based on procar project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,76 @@
 import React from 'react';
-
 import '../css/App.css';
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { DealerPage } from './screens/DealerPage';
+import { CommunityPage } from './screens/CommunityPage';
+import { OrdersPage } from './screens/OrdersPage';
+import { MemberPage } from './screens/MemberPage';
+import { HelpPage } from './screens/HelpPage';
+import { LoginPage } from './screens/LoginPage';
+import { HomePage } from './screens/HomePage';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dealer">DealerPage</Link>
+            </li>
+            <li>
+              <Link to="/community">CommunityPage</Link>
+            </li>
+            <li>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">MemberPage</Link>
+            </li>
+            <li>
+              <Link to="/help">HelpPage</Link>
+            </li>
+            <li>
+              <Link to="/login">LoginPage</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+        <Route path="/dealer">
+            <DealerPage />
+          </Route>
+          <Route path="/community">
+            <CommunityPage />
+          </Route>
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <MemberPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
---
+++ src/app/screens/DealerPage/index.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export function DealerPage() {
+    return <Container>DealerPage</Container>
+}
